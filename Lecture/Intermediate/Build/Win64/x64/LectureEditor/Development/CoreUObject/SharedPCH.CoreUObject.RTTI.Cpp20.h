9dd599768d56c000f76cd93a4315223b495904cc
Ektoba <wypka78@gmail.com>
2025-04-25 21:41:53 +0900

    맵추가

--- Lecture/Intermediate/Build/Win64/x64/LectureEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/UnrealPratice/Lecture/Lecture/Intermediate/Build/Win64/x64/LectureEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- Lecture/Intermediate/Build/Win64/x64/LectureEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/UnrealPratice/Lecture/Lecture/Intermediate/Build/Win64/x64/LectureEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
